080d33e321875e580dc44a51ebf434fe254066d9
artartur <artur.souza@tds.company>
2025-07-13 16:32:54 -0300

    feat(saga): implement saga to fetch API data

---
+++ src/store/sagas/dataSaga.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { call, delay, put, takeEvery } from "redux-saga/effects";
+
+import {
+  fetchDataFailure,
+  fetchDataSucess,
+  setLoading
+}
+  from "../slices/dataSlice";
+
+import type { AxiosResponse } from "axios";
+import type { IData } from "../../interfaces/data.types";
+
+function* fetchDataSaga() {
+  try {
+    yield put(setLoading(true));
+
+    yield delay(500);
+
+    const response: AxiosResponse<IData[]> = yield call(
+      axios.get,
+      'https://jsonplaceholder.typicode.com/users'
+    )
+
+    yield put(fetchDataSucess(response.data));
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : 'Error fetching datas ocurred';
+    yield put(fetchDataFailure(errorMessage));
+  }
+}
+
+export default function* dataSaga() {
+  yield takeEvery('FETCH_DATA', fetchDataSaga);
+}
+
+export const fetchData = () => ({ type: 'FETCH_DATA' });
